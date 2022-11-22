0dec8a21440b59f99efb2c4575034c31109bd79f
Bieo <bieo1@mail.ru>
2022-11-22 20:16:51 +0300

    Провели рефакторинг кода

--- app.js
+++ app.js
@@ -23,35 +23,35 @@ squares.forEach((item) => {
     })
 })
 
+function winResult(arr, winnerLetter) {
+    arr.forEach(i => {
+        squares[i].style.backgroundColor = '#1c1c1c'
+    })
+    result.innerHTML = `<p>win ${winnerLetter}</p>`
+    end = true
+}
+
 function win() {
     let winnerLetter = 'x'
     if (count % 2 != 0) {
         winnerLetter = 'o'
     }
     if (squares[0].innerHTML == squares[1].innerHTML && squares[1].innerHTML == squares[2].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([0, 1, 2], winnerLetter)
     } else if (squares[3].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[5].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([3, 4, 5], winnerLetter)
     } else if (squares[6].innerHTML == squares[7].innerHTML && squares[7].innerHTML == squares[8].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([6, 7, 8], winnerLetter)
     } else if (squares[0].innerHTML == squares[3].innerHTML && squares[3].innerHTML == squares[6].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([0, 3, 6], winnerLetter)
     } else if (squares[1].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[7].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([1, 4, 7], winnerLetter)
     } else if (squares[2].innerHTML == squares[5].innerHTML && squares[5].innerHTML == squares[8].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([2, 5, 8], winnerLetter)
     } else if (squares[0].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[8].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([0, 4, 8], winnerLetter)
     } else if (squares[2].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[6].innerHTML) {
-        result.innerHTML = `<p>win ${winnerLetter}</p>`
-        end = true
+        winResult([2, 4, 6], winnerLetter)
     } else if (count == 8) {
         result.innerHTML = `<p>Ничья</p>`
         end = true
